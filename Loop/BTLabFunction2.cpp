3ad510f17f2bcffa0a2aa0172a1f1be6eab3ec83
CyrusWolf <cyberdroid133@gmail.com>
2022-11-28 16:02:57 +0700

    new file:   Loop/BTLabFunction2.cpp 	new file:   Loop/BTLabFunction2.exe 	modified:   Loop/For2.exe 	new file:   Loop/tempCodeRunnerFile.cpp

---
+++ Loop/BTLabFunction2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int arearectangle(int a, int b){
+    return a * b;
+}
+
+int square(int x){
+    return x * x;
+}
+double factorial(int x)
+{
+    if (x == 1)
+    return 1;
+    return x * factorial(x - 1);
+}
+int main(){
+    int a,b,x, GiaiThua = 1;
+    cout<<"Nhap so 1st: ";
+    cin>>a;
+    cout<<"Nhap so 2nd: ";
+    cin>>b;
+    cout<<"Nhap x: ";
+    cin>>x;
+    int DienTich = arearectangle(a,b);
+    cout<<"\nDien tich hinh chu nhat la: "<<DienTich;
+    int BinhPhuong = square(x);
+    cout<<"\nBinh phuong x la: "<<BinhPhuong;
+    double Giaithua = factorial(x);
+    cout<<"\nGiai thua cua x la: "<<Giaithua;
+
+return 0;
+}
