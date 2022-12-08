69ef54d3121005e497294749cf0aafcf6b529ab6
CyrusWolf <cyberdroid133@gmail.com>
2022-12-08 15:04:19 +0700

    new file:   BT Lab Lap trinh Tong hop 1.pdf 	new file:   File/BTFile1.cpp

---
+++ File/BTFile1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int thuthaptruocthue (int luong, int phucap){
+    return luong + phucap;
+}
+int thuthapchiuthue (int thuthaptruocthue){
+    return thuthaptruocthue - 9000;
+}
+int main (){
+    int luong = 0, phucap = 0
+    int thunhaptruocthue;
+    cout<<"Nhap luong cua ban: ";
+    cin>>luong;
+    cout<<"Nhap tien phu cap: "
+    cin>>phucap;
+
+    if (thuthapchiuthue <= 5000)
+
+}
