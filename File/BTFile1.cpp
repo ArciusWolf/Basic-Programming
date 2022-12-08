deadd568fd8dcc49948a3b8299fb14de82d2bad3
CyrusWolf <cyberdroid133@gmail.com>
2022-12-08 16:16:56 +0700

    modified:   File/BTFile1.cpp 	new file:   File/BTFile1.exe 	new file:   File/tempCodeRunnerFile.cpp

--- File/BTFile1.cpp
+++ File/BTFile1.cpp
@@ -4,17 +4,35 @@ using namespace std;
 int thuthaptruocthue (int luong, int phucap){
     return luong + phucap;
 }
-int thuthapchiuthue (int thuthaptruocthue){
-    return thuthaptruocthue - 9000;
-}
 int main (){
-    int luong = 0, phucap = 0
-    int thunhaptruocthue;
+    int luong = 0, phucap = 0;
+    int thuthaptruocthue;
+    thuthaptruocthue = luong + phucap;
+    int thuthapchiuthue;
+    thuthapchiuthue = thuthaptruocthue - 9000;
+    
+    float thunhaptruocthue;
+    float thuthapthuclinh;
     cout<<"Nhap luong cua ban: ";
     cin>>luong;
-    cout<<"Nhap tien phu cap: "
+    cout<<"Nhap tien phu cap: ";
     cin>>phucap;
 
-    if (thuthapchiuthue <= 5000)
-
-}
+    if (thuthapchiuthue <= 5000) {
+        thuthapthuclinh = thuthapchiuthue * 0.05;
+    }
+        else if (thuthapchiuthue >= 5001 && thuthapchiuthue <= 10000){
+            thuthapthuclinh = thuthapchiuthue * 0.1;
+        }
+        else if (thuthapchiuthue >= 10001 && thuthapchiuthue <= 15000){
+            thuthapthuclinh = thuthapchiuthue * 0.15;
+        }
+        else if (thuthapchiuthue >= 15001 && thuthapchiuthue <= 20000){
+            thuthapthuclinh = thuthapchiuthue * 0.2;
+        }
+        else if (thuthapchiuthue > 20000){
+            thuthapthuclinh = thuthapchiuthue * 0.25;
+        }  
+            cout<<"Thu nhap thuc linh cua ban la: "<<thuthapthuclinh;
+    return 1;
+    }
