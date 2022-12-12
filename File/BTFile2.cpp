2507f6b5fadd01993829db2815452453e9871210
CyrusWolf <cyberdroid133@gmail.com>
2022-12-12 16:30:47 +0700

    new file:   Exception/~$ception.docx 	modified:   File/BTFile2.cpp 	modified:   File/BTFile2.exe 	new file:   File/data.txt

--- File/BTFile2.cpp
+++ File/BTFile2.cpp
@@ -5,9 +5,20 @@ using namespace std;
 
 int main(){
     string data;
-    ifstream file("work.txt");
-    while (getline(file, data)){
-        cout<<"\n"<<data;
+    ofstream file("data.txt");
+    for (int i=1; i<=10; i++){
+        cout<<"\nNhap du lieu: ";
+        getline(cin, data);
+        cout<<data<<"\n";
+        file<<data;
+    }
+
+    file.close();
+
+    string data1;
+    ifstream file1("data.txt");
+    while (getline(file1, data1)){
+        cout<<data1<<"\n";
     }
 
         file.close();
